287e393a8744dc80d3e97ff996a83973cf280c31
Nikitos <pocketkurt@gmail.com>
2020-04-02 17:44:21 +0300

    null
    null

--- Test.cpp
+++ Test.cpp
@@ -1,28 +1,3 @@
-﻿#include <iostream>
+﻿bool isEven(long long int value) { return value % 2 == 0; }
 
-
-using namespace std;
-
-bool isEven(unsigned long long int value) { return value % 2 == 0; }
-
-// скобки для подстраховки, т.к. по приоритету операций битовые операторы ниже приоритетом чем операторы отношений
-bool isEven2(unsigned long long int value) { return (value & 1) == 0; }
-
-
-int main() {
-	if (isEven(123141241241235348)) {
-		cout << "Chetnoe";
-	}
-	else {
-		cout << "Ne chetnoe";
-	}
-	cout << "\n------------------------------------\n";
-	if (isEven2(123141241241235348)){
-		cout << "Chetnoe";
-	}
-	else {
-		cout << "Ne chetnoe";
-	}
-
-	return 0;
-}
+bool isEven2(long long int value) { return (value & 1) == 0; }
